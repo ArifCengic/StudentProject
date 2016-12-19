046d75f5e8b327ca187430200d693d8baf4746e1
acengic <acengic@gmail.com>
2016-12-19 18:03:04 +0100

    Initial commit

---
+++ StudentDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentDemo
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			List<Student> students = new List<Student> {
+				new Student { ime = "Ana", ocjene = new int[] {2,5,0,7,5,4,3,6}, dob = new DateTime(1985,11,3) },
+				new Student() { ime = "Jim", ocjene = new int[] {8,5,3,7,5,4,9,6}, dob = new DateTime(1995,12,3) }
+			};
+
+			double a = students[0].d * 0.1;
+			Student s = new Stipendista();
+			s.ime = "Amy";
+			s.dob = students[0].dob;
+			s.ocjene = students[0].ocjene;
+			s.ocjene[2] = 10;
+
+			students.Add(s);
+			decimal sum = 0;
+			foreach (Student st in students)
+			{
+				Console.WriteLine(st.toString());
+				sum += st.calculatePayment();
+			}
+
+			double dsum = 0;
+			students.ForEach(st => { Console.WriteLine(st.toString()); dsum += dsum + st.getAverage(); });
+			students.TrueForAll(x => x.ime.Length == 3);
+
+			var fs = students.Find(x  => x.ime == "Ana");
+			Console.WriteLine(fs.toString());
+			if (a == 1) Console.WriteLine("Equal value");
+			else Console.WriteLine("Not Equal value");
+			Console.WriteLine("Total " + sum + "\n" + "--- Done! ---");
+			Console.ReadLine();
+
+		}
+	}
+}
---
+++ StudentDemo/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace StudentDemo
+{
+	public class Student
+	{
+			public string ime;
+			public int[] ocjene = new int[10];
+			int visinaCM;
+			string fakultet;
+			public DateTime dob;
+		decimal skolarina;
+		public double d = 10;
+		bool isMale;
+
+		public double getAverage()
+		{
+			double sum = 0;
+			for (int i = 0; i < ocjene.Length; i++)
+			{
+				sum = sum + ocjene[i];
+			}
+
+			return sum / ocjene.Length;
+		}
+
+		 public decimal calculatePayment()
+		{
+			return 2500;
+		}
+		public string toString(){
+			string res =  "Student " + ime + " rodjen " + dob.ToLongDateString() + " prosjek " + getAverage();
+			// res = res + " ocjene: ";
+			// foreach (var o in this.ocjene) res = res + o + ", ";
+			return res;
+		}
+	}
+
+	public class Stipendista : Student {
+	 public decimal calculatePayment()
+		{
+			if (getAverage() >= 4.5) return 100;
+			else return 2500;
+		}
+	}
+
+}
+
+
