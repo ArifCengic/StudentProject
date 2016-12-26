8d84de7ca23c9a195f7ad9a4301e3ba03aa41a09
ArifCengic <cengica@hotmail.com>
2016-12-26 20:44:05 +0000

    LINQ

--- StudentDemo/Program.cs
+++ StudentDemo/Program.cs
@@ -1,19 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace StudentDemo
 {
-	class MainClass
+    public static class Extensions
+    {
+    public static string[] getWords(this string s)
+    {
+        string[] res = s.Split(new char[] { ',', ' ', '.' });
+        return res;
+    }
+    }
+    class MainClass
 	{
+       
+
 		public static void Main(string[] args)
 		{
+
+           //string[] r = Extensions.getWords("juce, danas sutra");
+           
 			List<Student> students = new List<Student> {
 				new Student { ime = "Ana", ocjene = new int[] {2,5,0,7,5,4,3,6}, dob = new DateTime(1985,11,3) },
 				new Student() { ime = "Jim", ocjene = new int[] {8,5,3,7,5,4,9,0}, dob = new DateTime(1995,12,3) },
-                new Stipendista() { ime = "Sam", ocjene = new int[] {8,5,8,7,5,4,9,0}, dob = new DateTime(1985,1,3) }
+                new Stipendista() { ime = "Ad", ocjene = new int[] {8,5,8,7,5,4,9,0}, dob = new DateTime(1980,1,3) },
+                new Stipendista() { ime = "Jeff", ocjene = new int[] {8,5,8,7,5,4,9,0}, dob = new DateTime(1985,1,3) },
+                new Stipendista() { ime = "Ali", ocjene = new int[] {8,9,8,7,7,4,9,0}, dob = new DateTime(1985,1,3) },
+                new Student() { ime = "Jane", ocjene = new int[] {8,5,5,7,5,4,9,7}, dob = new DateTime(1975,12,3) },
             };
 
             Stipendista.minProsjek = 5.2;
+            var some = new { name = "car", weight = 5000 };
+
+            var query = students
+                .Where(x => x.getAverage() > 2)
+                .OrderBy(x => x.ime)
+                .Take(3);
+       
+
+           // var fs = students.FindAll(x => x.ime.StartsWith("A") && x.getAverage() > 2);
+           //     fs.Sort( (x, y) => x.ime.CompareTo(y.ime));
+            //Func(Student,bool)
+            //bool Func (Student s)
+            decimal sum = 0;
+            int countStipendista = 0;
+            foreach (Student st in query)
+            {
+                if (st is Stipendista)
+                {
+                    countStipendista++;
+                    Stipendista stip = (Stipendista)st;
+                    Console.WriteLine(stip.toString());
+                }
+                else Console.WriteLine(st.toString());
+                sum += st.calculatePayment();
+            }
+
+            
+            //Console.WriteLine(fs.toString());
+
             /*****
             for(int i=0; i < 5; i++)
             {
@@ -47,22 +93,8 @@ namespace StudentDemo
                     break;
                 }
 		****/
-			decimal sum = 0;
-            int countStipendista = 0;
-			foreach (Student st in students)
-			{
-                if (st is Stipendista) countStipendista++;
-            
-                Console.WriteLine(st.toString());
-				sum += st.calculatePayment();
-			}
-			
 
-
-			//var fs = students.Find(x  => x.ime == "Ana");
-			//Console.WriteLine(fs.toString());
-			
-			Console.WriteLine("Total " + sum + "\n" + "--- Done! ---");
+            Console.WriteLine("Total " + sum + "\n" + "--- Done! ---");
 			Console.ReadLine();
 
 		}
--- StudentDemo/Student.cs
+++ StudentDemo/Student.cs
@@ -1,77 +1,84 @@
 ﻿using System;
 namespace StudentDemo
 {
-	public class Student
-	{
-		public string ime;
-		public int[] ocjene = new int[10];
-		int visinaCM;
-		string fakultet;
-		public DateTime dob;
-		decimal skolarina;
-		public double d = 10;
-		bool isMale;
+    public class Student
+    {
+        public string ime;
+        public int[] ocjene = new int[10];
+        int visinaCM;
+        string fakultet;
+        public DateTime dob;
+        decimal skolarina;
+        public double d = 10;
+        bool isMale;
 
-		public int getAttendance()
-		{
-			//Track attendance
-			return 100; //100%
-		}
-		public double getAverage()
-		{
-			double sum = 0;
-			for (int i = 0; i < ocjene.Length; i++)
-			{
-				sum = sum + ocjene[i];
-			}
+        public int getAttendance()
+        {
+            //Track attendance
+            return 100; //100%
+        }
+        public double getAverage()
+        {
+            double sum = 0;
+            for (int i = 0; i < ocjene.Length; i++)
+            {
+                sum = sum + ocjene[i];
+            }
 
-			return sum / ocjene.Length;
-		}
+            return sum / ocjene.Length;
+        }
 
-		virtual public decimal calculatePayment()
-		{
-			return 2600;
-		}
+        virtual public decimal calculatePayment()
+        {
+            return 2600;
+        }
 
-		public string toString(){
-			string res =  "Student " + ime + " rodjen " + dob.ToLongDateString() + " prosjek " + getAverage();
-			// res = res + " ocjene: ";
-			// foreach (var o in this.ocjene) res = res + o + ", ";
-			return res;
-		}
+        public string toString()
+        {
+            string res = "Student " + ime + " rodjen " + dob.ToLongDateString() + " prosjek " + getAverage();
+            // res = res + " ocjene: ";
+            // foreach (var o in this.ocjene) res = res + o + ", ";
+            return res;
+        }
 
-		//static public bool operator ==(Student a, Student b)
-		//{
-		//	if(a.ime == b.ime) return true;
-		//	else return false;
-		//}
+        //static public bool operator ==(Student a, Student b)
+        //{
+        //	if(a.ime == b.ime) return true;
+        //	else return false;
+        //}
 
-		//static public bool operator !=(Student a, Student b)
-		//{
-		//	if (a.ime == b.ime) return false;
-		//	else return true;
-		//}
+        //static public bool operator !=(Student a, Student b)
+        //{
+        //	if (a.ime == b.ime) return false;
+        //	else return true;
+        //}
 
-	}
+    }
 
-	public class Stipendista : Student {
-       public static double minProsjek;
+    public class Stipendista : Student
+    {
+        public static double minProsjek;
         public static decimal Payment;
 
         override public decimal calculatePayment()
-		{
-			if (getAverage() >= minProsjek) return Payment;
-			else return base.calculatePayment();
-		}
-	}
+        {
+            if (getAverage() >= minProsjek) return Payment;
+            else return base.calculatePayment();
+        }
 
-	public class Vandredni : Student
-	{
-		
-	}
+        new public string toString()
+        {
+            return base.toString() + " STIPENDISTA";
+        }
+
+        public class Vandredni : Student
+        {
+
+        }
 
 
 
+    }
 }
 
 
